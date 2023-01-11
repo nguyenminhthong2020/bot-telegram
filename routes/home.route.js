73ae5fedf81e6ca9400f7bfa2d1454eb95b314c9
nguyenminhthong2020 <1612674@student.hcmus.edu.vn>
2023-01-11 16:49:47 +0700

    .

--- routes/home.route.js
+++ routes/home.route.js
@@ -3,9 +3,12 @@ var homeController = require("../controllers/home.controller");
 
 let router = express.Router();
 
+const { TOKEN } = process.env;
+const URI = `/webhook/${TOKEN}`;
+
 let initWebRoutes = (app)=> {
     router.get("/", homeController.getHome);
-    //router.post("/bot", homeController.postBot);
+    router.post(URI, homeController.postBot);
     // router.post("/webhook", webhookController.postWebhook);
 
     return app.use("/", router);
